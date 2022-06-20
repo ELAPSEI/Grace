c719a5e4331e12695ccaf9cc9feb034850c3e488
ELAPSE <ELAPSEI@163.com>
2022-06-20 18:31:32 +0800

    模块整合

---
+++ mall-portal/src/test/java/com/elapse/portal/MallPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elapse.portal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ mall-security/src/test/java/com/elapse/security/MallSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elapse.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
