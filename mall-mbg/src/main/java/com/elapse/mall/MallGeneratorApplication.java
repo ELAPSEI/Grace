7e58cf8ba416f27941a9f858d4153830297eda84
ELAPSE <ELAPSEI@163.com>
2022-06-20 12:00:17 +0800

    springBoot整合Mybatis generator生成单表mapper

---
+++ mall-mbg/src/main/java/com/elapse/mall/MallGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.elapse.mall;
+
+import com.elapse.common.MallCommonApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallGeneratorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallGeneratorApplication.class, args);
+    }
+}
---
+++ note.md
@@ -0,0 +1,7 @@
+#elapse
+
+## springboot整合Mybatis
+- 1.找不到或无法加载主类
+>maven命令mvn idea model作用：重新构建.iml文件 在rebuild project重建项目
+>- 2.
+
